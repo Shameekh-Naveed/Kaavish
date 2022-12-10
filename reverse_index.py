a54fcc89a9181df17c07d3e0732feaa4af12088a
Jawad <jkabir789@gmail.com>
2022-12-11 00:20:03 +0500

    a single list for each document

--- reverse_index.py
+++ reverse_index.py
@@ -5,11 +5,12 @@ from nltk.corpus import stopwords
 
 def lemmatizer(pos_tags):
     lemmatizer = WordNetLemmatizer()
-    required = ['n','a', 'v', 'r']
     keywords = []
     for i in pos_tags:
-        if (i[1][0].lower() in required):
+        if(i[1][0].lower() in ['n','v','a','r']):
             keywords.append(lemmatizer.lemmatize(i[0],i[1][0].lower()))
+        else:
+            keywords.append(lemmatizer.lemmatize(i[0]))
     return keywords
 
 
@@ -36,13 +37,32 @@ keyword_dict = {}
 
 for i in range(len(json_data)):
     doc_list = tokenize(json_data[i]['title']+" "+json_data[i]['content'])
-    for j in doc_list:
-      if(j not in keyword_dict and j not in stop_words):
-        keyword_list.append(j)
-        keyword_dict[j] = []
-        keyword_dict[j].append((i,doc_list.count(j)))
-      elif(j in keyword_dict):
-        keyword_dict[j].append((i,doc_list.count(j)))
+
+    #commented this out to increase accuracy
+
+    # for j in doc_list:
+    #   if(j not in keyword_dict and j not in stop_words):
+    #     keyword_list.append(j)
+    #     keyword_dict[j] = []
+    #     keyword_dict[j].append((i,doc_list.index(j)))
+    #   elif(j in keyword_dict):
+    #     keyword_dict[j].append((i,doc_list.count(j)))
+    # print(f"Document {i} done")
+    # if (i==500):
+    #     break
+    for j in range(len(doc_list)):
+        if(doc_list[j] not in keyword_dict and doc_list[j] not in stop_words):
+            keyword_list.append(doc_list[j])
+            keyword_dict[doc_list[j]] = []
+            keyword_dict[doc_list[j]].append([i,j])
+        elif(doc_list[j] in keyword_dict):
+            flag = False
+            for k in range(len(keyword_dict[doc_list[j]])):
+                if(keyword_dict[doc_list[j]][k][0] == i):
+                    keyword_dict[doc_list[j]][k].append(j)
+                    flag = True
+            if flag == False:
+                keyword_dict[doc_list[j]].append([i,j])
     print(f"Document {i} done")
     if (i==500):
         break
@@ -50,6 +70,6 @@ for i in range(len(json_data)):
 keyword_dict_json = json.dumps(keyword_dict, indent=1)
 print("Done")
 
-with open("reverse.json","w") as myFile:
+with open("reverse2.json","w") as myFile:
     myFile.write(keyword_dict_json)
     
